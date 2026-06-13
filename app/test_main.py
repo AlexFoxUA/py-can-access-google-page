c98e374c8eedf6a92673fd5e212b81fed2a71fda
Alex <alex@example.com>
2026-06-13 17:03:54 +0300

    finish2

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,13 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import app.main as main
 
 
 @patch('app.main.valid_google_url')
 @patch('app.main.has_internet_connection')
-def test_valid_url_and_connection_exists(mock_connection, mock_url):
+def test_valid_url_and_connection_exists(
+    mock_connection: MagicMock,
+    mock_url: MagicMock
+) -> str:
     mock_url.return_value = True
     mock_connection.return_value = True
 
@@ -14,7 +17,10 @@ def test_valid_url_and_connection_exists(mock_connection, mock_url):
 
 @patch('app.main.valid_google_url')
 @patch('app.main.has_internet_connection')
-def test_cannot_access_if_only_connection(mock_connection, mock_url):
+def test_cannot_access_if_only_connection(
+    mock_connection: MagicMock,
+    mock_url: MagicMock
+) -> str:
     mock_url.return_value = True
     mock_connection.return_value = False
 
@@ -24,7 +30,10 @@ def test_cannot_access_if_only_connection(mock_connection, mock_url):
 
 @patch('app.main.valid_google_url')
 @patch('app.main.has_internet_connection')
-def test_cannot_access_if_only_valid_url(mock_connection, mock_url):
+def test_cannot_access_if_only_valid_url(
+    mock_connection: MagicMock,
+    mock_url: MagicMock
+) -> str:
     mock_url.return_value = False
     mock_connection.return_value = True
 
@@ -34,7 +43,10 @@ def test_cannot_access_if_only_valid_url(mock_connection, mock_url):
 
 @patch('app.main.valid_google_url')
 @patch('app.main.has_internet_connection')
-def test_invalid_url_without_connection(mock_connection, mock_url):
+def test_invalid_url_without_connection(
+    mock_connection: MagicMock,
+    mock_url: MagicMock
+) -> str:
     mock_connection.return_value = False
     mock_url.return_value = False
 
