8e85182109939f50384e5be79576db23a5139a07
Alex <alex@example.com>
2026-06-13 17:20:47 +0300

    finish5

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ import app.main as main
 def test_valid_url_and_connection_exists(
     mock_connection: MagicMock,
     mock_url: MagicMock
-):
+) -> str:
     mock_url.return_value = True
     mock_connection.return_value = True
 
@@ -20,7 +20,7 @@ def test_valid_url_and_connection_exists(
 def test_cannot_access_if_only_connection(
     mock_connection: MagicMock,
     mock_url: MagicMock
-):
+) -> str:
     mock_url.return_value = True
     mock_connection.return_value = False
 
@@ -33,7 +33,7 @@ def test_cannot_access_if_only_connection(
 def test_cannot_access_if_only_valid_url(
     mock_connection: MagicMock,
     mock_url: MagicMock
-):
+) -> str:
     mock_url.return_value = False
     mock_connection.return_value = True
 
@@ -46,7 +46,7 @@ def test_cannot_access_if_only_valid_url(
 def test_invalid_url_without_connection(
     mock_connection: MagicMock,
     mock_url: MagicMock
-):
+) -> str:
     mock_connection.return_value = False
     mock_url.return_value = False
 
